234a2296be2eb981778de4e31fb8c7b028f70bbd
Sergio Santos <sergi@Victus>
2025-06-04 17:23:31 +0100

    using geany_8

--- TEST_4/Test_4.c
+++ TEST_4/Test_4.c
@@ -30,6 +30,26 @@
 #define AREA(l, b) (l * b)
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
+typedef volatile union{
+	struct{
+		uint8_t b0:1;
+		uint8_t b1:1;
+		uint8_t b2:1;
+		uint8_t b3:1;
+		uint8_t b4:1;
+		uint8_t b5:1;
+		uint8_t b6:1;
+		uint8_t b7:1;
+	}par;
+	uint8_t reg;
+} Byte;
+
+typedef struct{
+	Byte* A;
+	Byte* B;
+	Byte* C;
+} List;
+
 void delay(int number_of_seconds);
 static FUNC func;
 
@@ -40,20 +60,22 @@ printf("Running program - %s\nwith - %d arguments\n\n", argv[0], argc);
 
 func = FUNCenable();
 char* cmd = NULL;
-unsigned int choice;
 /*****************/
-char vector[100] = {0};
-char received[100] = {0};
-circbuff buffer = CIRCBUFFenable( 10, vector );
+uint8_t vector[100] = {0};
 
-buffer.puts( &buffer, "Hellosergio" );
-//printf( "string: %s", vector );
+List *lst;
+lst->A = (Byte*)vector;
+lst->B = (Byte*)vector+1;
+lst->C = (Byte*)NULL;
+Byte *byte;
+byte = (Byte*)vector;
+vector[0]=10;
+vector[1]=255;
 
 while ass
 {
 	printf("\nEnter i Data : ");
 	cmd=func.fltos(stdin);
-	choice = func.getnumv2(cmd);
 	if(!strcmp(cmd,"quit") || !strcmp(cmd,"q")){
 		goto end;
 	}
@@ -65,16 +87,38 @@ while ass
 	if( !strcmp(cmd,"run") || !strcmp(cmd,"r") ){  // one shot testing
 /****************************************************************************************************************************/
 /****************************************************************************************************************************/
-for(uint8_t i =9; i; i--){
-//char c = getchar();
 
-//buffer.put(&buffer, c);
 
-//buffer.gets(&buffer,received);
-//printf( "string: %s\n", vector );
-printf( "string: %c\n", buffer.get(&buffer) );
-//printf( "string: %s\n", received );
-}
+printf("byte->reg: %d\n", byte->reg);
+printf("byte->par.b7: %d\n", byte->par.b7);
+printf("byte->par.b6: %d\n", byte->par.b6);
+printf("byte->par.b5: %d\n", byte->par.b5);
+printf("byte->par.b4: %d\n", byte->par.b4);
+printf("byte->par.b3: %d\n", byte->par.b3);
+printf("byte->par.b2: %d\n", byte->par.b2);
+printf("byte->par.b1: %d\n", byte->par.b1);
+printf("byte->par.b0: %d\n", byte->par.b0);
+printf("\n");
+printf("lst->A->reg: %d\n", lst->A->reg);
+printf("lst->A->par.b7: %d\n", lst->A->par.b7);
+printf("lst->A->par.b6: %d\n", lst->A->par.b6);
+printf("lst->A->par.b5: %d\n", lst->A->par.b5);
+printf("lst->A->par.b4: %d\n", lst->A->par.b4);
+printf("lst->A->par.b3: %d\n", lst->A->par.b3);
+printf("lst->A->par.b2: %d\n", lst->A->par.b2);
+printf("lst->A->par.b1: %d\n", lst->A->par.b1);
+printf("lst->A->par.b0: %d\n", lst->A->par.b0);
+printf("\n");
+printf("lst->B->reg: %d\n", lst->B->reg);
+printf("lst->B->par.b7: %d\n", lst->B->par.b7);
+printf("lst->B->par.b6: %d\n", lst->B->par.b6);
+printf("lst->B->par.b5: %d\n", lst->B->par.b5);
+printf("lst->B->par.b4: %d\n", lst->B->par.b4);
+printf("lst->B->par.b3: %d\n", lst->B->par.b3);
+printf("lst->B->par.b2: %d\n", lst->B->par.b2);
+printf("lst->B->par.b1: %d\n", lst->B->par.b1);
+printf("lst->B->par.b0: %d\n", lst->B->par.b0);
+
 
 /****************************************************************************************************************************/
 /****************************************************************************************************************************/
