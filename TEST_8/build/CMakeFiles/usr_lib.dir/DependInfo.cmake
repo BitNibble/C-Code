d80049576dfb9dc964abaa9e9455b405a068add8
Sergio Santos <sergi@Victus>
2025-06-15 20:54:52 +0100

    try out cmake

---
+++ TEST_8/build/CMakeFiles/usr_lib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/C-Code/TEST_8/user_lib/src/PCficheiro.c" "CMakeFiles/usr_lib.dir/user_lib/src/PCficheiro.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/PCficheiro.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/PCfunction.c" "CMakeFiles/usr_lib.dir/user_lib/src/PCfunction.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/PCfunction.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/PClili.c" "CMakeFiles/usr_lib.dir/user_lib/src/PClili.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/PClili.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/circbuffer.c" "CMakeFiles/usr_lib.dir/user_lib/src/circbuffer.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/circbuffer.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/explode.c" "CMakeFiles/usr_lib.dir/user_lib/src/explode.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/explode.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/poop.c" "CMakeFiles/usr_lib.dir/user_lib/src/poop.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/poop.c.o.d"
+  "/mnt/d/C-Code/TEST_8/user_lib/src/procedures.c" "CMakeFiles/usr_lib.dir/user_lib/src/procedures.c.o" "gcc" "CMakeFiles/usr_lib.dir/user_lib/src/procedures.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TEST_8/build/CMakeFiles/usr_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libusr_lib.a"
+)
